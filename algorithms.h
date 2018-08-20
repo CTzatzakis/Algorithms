593acd4fdaa1fc2553a227d039668a1caa192415
Chris Tzatzakis <33452504+CTzatzakis@users.noreply.github.com>
2018-08-20 19:27:35 +0300

    Update algorithms.h

--- algorithms.h
+++ algorithms.h
@@ -223,7 +223,7 @@ Selection sort is an O(n2) sorting algorithm, simple in its programming and it c
 void selectionSort(int arr[], int n) 
 {
 	int i, j, minIndex, tmp;    
-	
+
 	for (i = 0; i < n - 1; i++) 
 	{
 		minIndex = i;
@@ -293,3 +293,86 @@ void quickSort(int arr[], int left, int right)
 	if (i < right)
 		quickSort(arr, i, right);
 }
+
+//Merge Sort
+/*
+ It divides input array in two halves, calls itself for the two halves and then merges the two sorted halves. 
+ The merge() function is used for merging two halves. The merge(arr, l, m, r) is key process that assumes 
+ that arr[l..m] and arr[m+1..r] are sorted and merges the two sorted sub-arrays into one. 
+ 
+ Merges two subarrays of arr[].
+ First subarray is arr[l..m]
+ Second subarray is arr[m+1..r]
+*/
+
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy the remaining elements of L[], if there
+       are any */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy the remaining elements of R[], if there
+       are any */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l+(r-l)/2;
+ 
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+/========================================================/
